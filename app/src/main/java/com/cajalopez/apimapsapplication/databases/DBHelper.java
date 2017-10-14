3ddc3af1f2260617a8596c9604e101f3d5cb2f9d
74-PC\74 <carloslopez8613@gmail.com>
2017-10-14 07:58:08 -0500

    dbhelper

---
+++ app/src/main/java/com/cajalopez/apimapsapplication/databases/DBHelper.java
@@ -0,0 +1,52 @@
+package com.cajalopez.apimapsapplication.databases;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.orhanobut.logger.Logger;
+
+public class DBHelper extends SQLiteOpenHelper{
+
+    private final static int VERSION_DB = 1;
+    private final static String DB_NAME = "test";
+
+    public final static String TABLE_NAME = "chuck_norris";
+    public final static String COLUMN_ID = "_id";
+    public final static String COLUMN_SERVER_ID = "id";
+    public final static String COLUMN_NAME = "joke";
+    public final static String COLUMN_CAT = "categories";
+    private final SQLiteDatabase db;
+
+    public DBHelper(Context context) {
+        super(context, DB_NAME, null, VERSION_DB);
+        db = this.getReadableDatabase();
+    }
+
+    public void deleteRows(int id){
+        String whereClause = "(" + COLUMN_ID + " = %s)";
+        String[] whereArgs = {String.valueOf(id)};
+        db.delete(TABLE_NAME, null, null);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE_NAME + " ("
+                + COLUMN_ID + " integer PRIMARY KEY not null, "
+                + COLUMN_SERVER_ID + " INTEGER, "
+                + COLUMN_NAME + " TEXT "
+                //+ COLUMN_CAT + " TEXT "
+                + ")";
+        db.execSQL(query);
+        Logger.d("Base de datos creada, %s", TABLE_NAME);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        String query = "DROP TABLE IF EXIST " + TABLE_NAME;
+        db.execSQL(query);
+        onCreate(db);
+    }
+}
